e0fb425028451998b9ddb85e4a90d81e57034eca
Marcelo Júnior <macelo17@gmail.com>
2023-08-23 15:45:59 -0300

    V_2AULAS_1,9

--- LogicaProgramação.java/src/LogicaProgramacao/OlaMundo.java
+++ LogicaProgramação.java/src/LogicaProgramacao/OlaMundo.java
@@ -1,9 +1,9 @@
 package LogicaProgramacao;
 public class OlaMundo {   //classe, tudo a partir daqui será executado
     public static void main(String[] args) {  //metodo (main) que estar dentro de uma class
-        System.out.println("Olá, Mundo!");
-        System.out.println("Aprendendo no Bootcamp Santander - DIO");
-        System.out.println("JAVA é uma mãe  kkkkk"); // ; é quando acaba a instrução do código
+        System.out.println("\n\nOlá, Mundo!\n\n");
+        System.out.println("Aprendendo no Bootcamp Santander - DIO\n\n");
+        System.out.println("JAVA é uma mãe  kkkkk \n\n\nComo é dificil aprender JAVA\n\n"); // ; é quando acaba a instrução do código
           
     }
 }
