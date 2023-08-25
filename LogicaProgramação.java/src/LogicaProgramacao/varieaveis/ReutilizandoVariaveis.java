126a80cdd72f9d56c7bd4ba3fcd13f76dce3529d
Marcelo Júnior <macelo17@gmail.com>
2023-08-25 17:13:38 -0300

    Aulas10,13

---
+++ LogicaProgramação.java/src/LogicaProgramacao/varieaveis/ReutilizandoVariaveis.java
@@ -0,0 +1,23 @@
+package LogicaProgramacao.varieaveis;
+
+/* crie uma variavel que irá guardar um valor chamado salario 
+ 
+ calcule a porcentagem desse salario em, 30, 15 e 5 porcento
+ 
+ a cada ver que calcular, guardar em uma variavel, imprima a variavel e reutilize a variavel que guarde o resultado para novo calculo
+ */
+
+public class ReutilizandoVariaveis {
+    public static void main(String[] args) {
+        double salario = 10000;
+        double resultado = salario * 0.33;
+        System.out.println("33% é :"  +  resultado);
+
+        resultado = salario * 0.10;
+        System.out.println("10% é "+ resultado);
+
+        resultado = salario * 0.05;
+        System.out.println("5% é "+ resultado);
+    }
+    
+}
---
+++ LogicaProgramação.java/src/LogicaProgramacao/varieaveis/TestandoVariaveis.java
@@ -0,0 +1,14 @@
+package LogicaProgramacao.varieaveis;
+
+public class TestandoVariaveis {
+
+    public static void main(String[] args) {
+        char masculino = 'A';
+        char feminino = 'A';
+        String masculino2 = "A";
+        String feminino2 = "A";
+        
+        System.out.println(masculino2+feminino2);
+    }
+    
+}
