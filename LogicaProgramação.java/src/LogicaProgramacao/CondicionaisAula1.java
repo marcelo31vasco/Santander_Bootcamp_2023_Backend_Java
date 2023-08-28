34c2616ebb3038efe897eff42111543ea64a4009
Marcelo Júnior <macelo17@gmail.com>
2023-08-28 17:11:42 -0300

    Aula14

---
+++ LogicaProgramação.java/src/LogicaProgramacao/CondicionaisAula1.java
@@ -0,0 +1,22 @@
+package LogicaProgramacao;
+
+public class CondicionaisAula1 {
+    public static void main(String[] args) {
+
+
+boolean condicao = false;
+
+/*Testes com e sem a variavel. Sendo True ou False 
+ 
+  Testando o ELSE
+*/
+
+        if(condicao){
+            System.out.println("Dentro do if");
+        }else{
+            System.out.println("Dentro do Else");
+        }
+        System.out.println("Fora do IF");
+    }
+}
+
