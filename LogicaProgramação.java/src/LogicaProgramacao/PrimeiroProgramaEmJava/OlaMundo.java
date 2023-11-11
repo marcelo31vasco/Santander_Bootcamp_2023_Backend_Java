1409e096611024f6fd656eb253b33f704050899f
marcelo31vasco <macelo17@gmail.com>
2023-11-11 16:16:39 -0300

    V11Ferias

---
+++ LogicaProgramação.java/src/LogicaProgramacao/PrimeiroProgramaEmJava/OlaMundo.java
@@ -0,0 +1,11 @@
+package LogicaProgramacao.PrimeiroProgramaEmJava;
+
+public class OlaMundo { // classe, tudo a partir daqui será executado
+    public static void main(String[] args) { // metodo (main) que estar dentro de uma class
+        System.out.println("\n\nOlá, Mundo!\n\n");
+        System.out.println("Aprendendo no Bootcamp Santander - DIO\n\n");
+        System.out.println("JAVA é uma mãe  kkkkk \n\n\nComo é dificil aprender JAVA\n\n"); // ; é quando acaba a
+                                                                                            // instrução do código
+
+    }
+}
