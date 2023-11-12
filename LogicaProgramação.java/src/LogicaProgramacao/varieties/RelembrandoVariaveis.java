aa247128be7a6da5a11e6328fe5932451c31ed2c
marcelo31vasco <macelo17@gmail.com>
2023-11-12 14:01:02 -0300

    V12/11

---
+++ LogicaProgramação.java/src/LogicaProgramacao/varieties/RelembrandoVariaveis.java
@@ -0,0 +1,17 @@
+package LogicaProgramacao.varieties;
+
+public class RelembrandoVariaveis {
+
+    public static void main(String[] args) {
+        String primeroNome = "Marcelo";
+        String segundoNome = "Junior";
+
+        String nomeComplete = nomeComplete(primeroNome, segundoNome);
+
+    }
+
+    public static String nomeComplete(String primeroNome, String segundoNome) {
+        return "Resultado do metodo" + primeroNome.concat(" ").concat(segundoNome);
+
+    }
+}
