8c74d28f373170a93713c33c110ad23ca4ef763b
marcelo31vasco <macelo17@gmail.com>
2023-11-13 15:22:02 -0300

    V13/11

---
+++ LogicaProgramação.java/src/LogicaProgramacao/Operadores/TasteBoolean.java
@@ -0,0 +1,31 @@
+package LogicaProgramacao.Operadores;
+
+public class TasteBoolean {
+
+    public static void main(String[] args) {
+
+        String nomeUm = "Marcelo";
+        String nomeDois = new String("Marcelo");
+
+        System.out.println(nomeUm.equals(nomeDois)); // quando for usar objetos usar o .equals
+
+        /*
+         * 
+         * int numero1 = 2;
+         * int numero2 = 3;
+         * 
+         * boolean simNao = numero1 == numero2;
+         * 
+         * if (numero1 < numero2) {
+         * System.out.println("cond verdadeira");
+         * }
+         * System.out.println("numero1 eh igual a numero2? " + simNao);
+         * 
+         * simNao = numero1 != numero2;
+         * 
+         * System.out.println("numero1 eh maior que numero2? " + simNao);
+         */
+
+    }
+
+}
---
+++ LogicaProgramação.java/src/LogicaProgramacao/TesteBoooleanDoiss.java
@@ -0,0 +1,18 @@
+package LogicaProgramacao;
+
+public class TesteBoooleanDoiss {
+
+    public static void main(String[] args) {
+
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+
+        if (condicao1 && (7 > 4)) {
+            System.out.println("As duas condicoes sao verdadeiras?");
+        }
+        if (condicao1 || condicao2) {
+            System.out.println("Uma das duas condicoes sao verdadeiras?");
+        }
+
+    }
+}
