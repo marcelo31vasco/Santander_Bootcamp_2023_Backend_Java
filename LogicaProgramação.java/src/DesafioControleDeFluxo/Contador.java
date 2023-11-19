c37e1f3c71075061fe1019f2811ed78cb8e7e674
marcelo31vasco <macelo17@gmail.com>
2023-11-19 15:56:58 -0300

    V3-19/11/23

--- LogicaProgramação.java/src/DesafioControleDeFluxo/Contador.java
+++ LogicaProgramação.java/src/DesafioControleDeFluxo/Contador.java
@@ -1,3 +1,9 @@
+/*
+ * DESCRIÇÃO
+Neste desafio de projeto, vamos explorar alguns cenários com fluxos condicionais, repetições e excepcionais. 
+Ao final, você poderá exercitar todo o conteúdo ministrado nas aulas implementando um pequeno sistema contador.
+ */
+
 package DesafioControleDeFluxo;
 
 import java.util.Scanner;
